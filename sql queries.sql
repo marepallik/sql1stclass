1528ef4eddab5608bb7b3fc74045103f33cc82f2
Keerthana marepalli <106551551+marepallik@users.noreply.github.com>
2023-04-27 13:33:36 +0530

    Add files via upload

---
+++ sql queries.sql
@@ -0,0 +1,3 @@
+SELECT * FROM rgm;
+INSERT INTO rgm VALUES(98789,"keerthana","rgmcet","atp"),(1234,"dency","nitcalicut","pld"),
+(5678,"neha","iiithyd","tpt"),(9012,"deepu","rggmm","atp"),(67767,"buela","svne","mpl");
